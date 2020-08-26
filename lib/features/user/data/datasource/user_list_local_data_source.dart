ee465900ea65998a1b640a9e0deeae10887d34f4
Oscar Ortiz Garcia <oortizg1@miumg.edu.gt>
2020-08-25 21:13:38 -0600

    configuracion de la informacion local terminada

---
+++ lib/features/user/data/datasource/user_list_local_data_source.dart
@@ -0,0 +1,43 @@
+
+
+import 'dart:convert';
+
+import 'package:ProyectoAnalisis2/features/user/data/models/usermodel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
+abstract class UserListLocalDataSource{
+
+ Future<User> getLastUserList();
+ Future<void> cacheUserList(User user);
+
+}
+
+
+const CACHED_USERLIST = 'CACHE_USER_LIST';
+
+class UserListLocalDataSourceImpl  implements UserListLocalDataSource{
+  final SharedPreferences sharedPreferences;
+
+  UserListLocalDataSourceImpl({@required this.sharedPreferences});
+
+
+ @override
+  Future<User> getLastUserList() {
+    final jsonString = sharedPreferences.getString(CACHED_USERLIST);
+    if (jsonString != null) {
+      return Future.value(User.fromJson(json.decode(jsonString)));
+    }
+ 
+  }
+
+
+  @override
+  Future<void> cacheUserList(User userCache) {
+    return sharedPreferences.setString(CACHED_USERLIST, json.encode(userCache.toJson()));
+
+  }
+
+ 
+  
+}
